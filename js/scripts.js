30fdeb17724c0a618e3ea3d6f0a137c107322ac7
lydiah1wachira <lydiahcedric@gmail.com>
2022-03-14 00:23:37 +0300

    create toggle functions for the development and product management sub-sections in the what we do section.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,22 @@
 // back-end logic.
 $(document).ready(function(){
-  $('#design-icon').click(function (){
-    $("#walrus-showing").toggle();
-    $("#walrus-hidden").toggle();
+  $('#design').click(function (){
+    $(".design-icon-show").toggle();
+    $(".design-icon-hide").toggle();
+    $
+  });
+
+  $('#development').click(function(){
+    $('.dev-icon-show').toggle();
+    $('.dev-icon-hide').toggle();
   })
-})
+
+  $('#product-management').click(function(){
+    $('.product-icon-show').toggle();
+    $('.product-icon-hide').toggle();
+  })
+});
+
+
+
+

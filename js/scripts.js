904a13a79049318ebf48cccfd896c21c822342f2
lydiah1wachira <lydiahcedric@gmail.com>
2022-03-14 19:20:50 +0300

    make the hover function using jQuery on all images in the portfolio section.

--- js/scripts.js
+++ js/scripts.js
@@ -19,9 +19,51 @@ $(document).ready(function(){
 
 $(document).ready(function(){
   $("#work1").mouseover(function(){
-    $("#overlay1").show();
+    $(".image_text1").show();
   }).mouseout(function(){
-    $("#overlay1").hide();
+    $(".image_text1").hide();
+  });
+
+  $("#work2").mouseover(function(){
+    $(".image_text2").show();
+  }).mouseout(function(){
+    $(".image_text2").hide();
+  });
+
+  $("#work3").mouseover(function(){
+    $(".image_text3").show();
+  }).mouseout(function(){
+    $(".image_text3").hide();
+  });
+
+  $("#work4").mouseover(function(){
+    $(".image_text4").show();
+  }).mouseout(function(){
+    $(".image_text4").hide();
+  });
+
+  $("#work5").mouseover(function(){
+    $(".image_text5").show();
+  }).mouseout(function(){
+    $(".image_text5").hide();
+  });
+
+  $("#work6").mouseover(function(){
+    $(".image_text6").show();
+  }).mouseout(function(){
+    $(".image_text6").hide();
+  });
+
+  $("#work7").mouseover(function(){
+    $(".image_text7").show();
+  }).mouseout(function(){
+    $(".image_text7").hide();
+  });
+
+  $("#work8").mouseover(function(){
+    $(".image_text8").show();
+  }).mouseout(function(){
+    $(".image_text8").hide();
   });
 });
 
